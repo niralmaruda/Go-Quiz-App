9205e2ed7984974220969b21698a87c1c1ce3f27
niralmaruda <85137760+niralmaruda@users.noreply.github.com>
2024-08-09 21:47:14 +0530

    - Restructure the quiz model

--- main.go
+++ main.go
@@ -8,14 +8,18 @@ import (
 
 func main() {
 	fmt.Println("Welcome to GO!")
-	ques := models.Quiz{
-		Question: "How are you",
-		Options: []models.Option{
-			{Option: "Good", Correct: true},
-			{Option: "Bad", Correct: false},
-			{Option: "Okay", Correct: false},
-			{Option: "Excellent", Correct: false},
-		},
+	quiz := models.Init()
+
+	for _, q := range quiz {
+		printQuestions(q)
 	}
-	fmt.Print(ques)
+}
+
+func printQuestions(quiz models.Quiz) {
+	quiz.PrintQuestion()
+	quiz.PrintOptions()
+
+	input := quiz.ReadInput()
+
+	quiz.CheckAnswer(input)
 }
--- models/quiz.go
+++ models/quiz.go
@@ -1,21 +1,87 @@
 package models
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
 type Quiz struct {
-	Question string
-	Options  []Option
+	question string
+	options  []Option
+	quesType int
 }
 
 type Option struct {
-	Option  string
-	Correct bool
-}
-
-type QuesType struct {
-	Radio    int
-	Checkbox int
+	option  string
+	correct bool
 }
 
 const (
-	Radio = iota
-	Checkbox
+	radio = iota
+	checkbox
 )
+
+const (
+	w = iota
+	s
+	a
+	d
+)
+
+var quiz []Quiz
+
+func Init() []Quiz {
+	quiz = []Quiz{{
+		question: "How are you",
+		options: []Option{
+			{option: "Good", correct: true},
+			{option: "Bad", correct: false},
+			{option: "Okay", correct: false},
+			{option: "Excellent", correct: false},
+		},
+		quesType: radio,
+	}}
+
+	return quiz
+}
+
+func (q Quiz) PrintQuestion() {
+	fmt.Println(q.question)
+}
+
+func (q Quiz) PrintOptions() {
+	for i, option := range q.options {
+		fmt.Printf("%d. %s\n", i+1, option.option)
+	}
+}
+
+func (q Quiz) CheckAnswer(input string) {
+	correct := false
+	if q.quesType == radio {
+		for i, option := range q.options {
+			if input == fmt.Sprint(i+1) && option.correct {
+				correct = true
+				break
+			}
+		}
+	} else if q.quesType == checkbox {
+		// Additional logic for checkbox type questions
+		fmt.Print("No implementation for checkbox yet.")
+	}
+
+	if correct {
+		fmt.Println("Correct!")
+	} else {
+		fmt.Println("incorrect!")
+	}
+}
+
+func (q Quiz) ReadInput() string {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter your choice: ")
+	input, _ := reader.ReadString('\n')
+	input = strings.TrimSpace(input)
+	return input
+}
